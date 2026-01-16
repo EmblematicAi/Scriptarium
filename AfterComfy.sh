efa8a60eb78c40c982ae05745236b5d7624b8d39
EmblematicAi <133839777+EmblematicAi@users.noreply.github.com>
2026-01-16 18:34:35 -0500

    Add files via upload

---
+++ AfterComfy.sh
@@ -0,0 +1,34 @@
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/theUpsider/ComfyUI-Logic --recursive
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/audioscavenger/save-image-extended-comfyui --recursive
+cd /workspace/ComfyUI/custom_nodes/save-image-extended-comfyui
+pip install -r requirements.txt
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/ssitu/ComfyUI_UltimateSDUpscale --recursive
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/adieyal/comfyui-dynamicprompts --recursive
+cd /workspace/ComfyUI/custom_nodes/comfyui-dynamicprompts 
+pip install -r requirements.txt
+#python -m pip install -r custom_nodes/comfyui-dynamicprompts/requirements.txt
+mkdir wildcards
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/ka-puna/comfyui-yanc/ --recursive
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/rgthree/rgthree-comfy --recursive
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/crystian/ComfyUI-Crystools --recursive
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-Crystools
+pip install -r requirements.txt
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/city96/ComfyUI-GGUF --recursive
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-GGUF
+pip install -r requirements.txt
+cd /workspace/ComfyUI/custom_nodes/
+#git clone https://github.com/mit-han-lab/ComfyUI-nunchaku /workspace/ComfyUI/custom_nodes/nunchaku_nodes --recursive
+git clone https://github.com/mit-han-lab/ComfyUI-nunchaku --recursive
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-nunchaku
+wget https://nunchaku.tech/cdn/nunchaku_versions.json -O "nunchaku_versions.json"
+mkdir /workspace/ComfyUI/user/default/workflows/
+cd /workspace/ComfyUI/user/default/workflows/
+wget https://raw.githubusercontent.com/nunchaku-ai/ComfyUI-nunchaku/refs/heads/main/example_workflows/install_wheel.json -O "install_wheel.json"
